949784a930e7524f7693a7ea72d18a6047062312
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-22 03:48:07 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,21 @@
-import {useEffect,useState} from "react";
-import API from "../api/axios";
-import TournamentCard from "../components/TournamentCard";
+import { Link } from "react-router-dom";
 
+function Home() {
+  return (
+    <div>
+      <h1>PR eSports</h1>
 
-function Home(){
+      <div>
+        <h2>Free Fire MAX Scrim</h2>
+        <p>Prize Pool: ₹1000</p>
+        <p>Status: Upcoming</p>
 
-const [tournaments,setTournaments]=useState([]);
-
-
-useEffect(()=>{
-
-API.get("/tournaments")
-.then(res=>{
-
-setTournaments(
-res.data.tournaments
-);
-
-});
-
-},[]);
-
-
-
-return (
-
-<div>
-
-<h1>
-PR eSports
-</h1>
-
-
-<h2>
-Upcoming Tournaments
-</h2>
-
-
-{
-tournaments?.map(t=>(
-
-<TournamentCard
-key={t._id}
-tournament={t}
-/>
-
-))
+        <Link to="/tournament/demo">
+          Join Tournament
+        </Link>
+      </div>
+    </div>
+  );
 }
-
-
-</div>
-
-)
-
-}
-
 
 export default Home;
