832668d1b2dd42ceeeec227c2f327aae7dc82fae
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-21 23:53:18 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.error(err);
+});
+
+app.get("/", (req, res) => {
+    res.send("PR eSports API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
