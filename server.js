ad6870aa6fc8e7f005278f1e1089c1e821d235d3
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-22 00:00:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+const tournamentRoutes = require("./routes/tournament");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tournament", tournamentRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running");
+});
 
 app.use(express.json());
 
