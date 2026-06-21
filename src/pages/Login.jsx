62035b3384b2bafa3903f7c70c3f16c74d5baa2f
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-22 02:39:59 +0530

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,29 @@
+function Login(){
+
+return (
+
+<div>
+
+<h1>
+Admin / Player Login
+</h1>
+
+
+<input placeholder="Email"/>
+
+<input placeholder="Password"/>
+
+
+<button>
+Login
+</button>
+
+
+</div>
+
+)
+
+}
+
+
+export default Login;
