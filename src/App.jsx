d39213dbd5d449060d4506e7e4889e058c3c369d
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-22 19:26:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Tournament from "./pages/Tournament";
 import Login from "./pages/Login";
+import Tournament from "./pages/Tournament";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/tournament/:id" element={<Tournament />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/tournament/:id" element={<Tournament />} />
       </Routes>
     </BrowserRouter>
   );
