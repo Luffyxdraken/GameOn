9d3efe65b916a408fdcaf26ababac69525499368
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-22 12:16:05 +0530

    Update TournamentCard.jsx

--- src/components/TournamentCard.jsx
+++ src/components/TournamentCard.jsx
@@ -5,16 +5,19 @@ function TournamentCard({ tournament }) {
         background: "#1e293b",
         padding: "20px",
         borderRadius: "10px",
-        marginBottom: "20px"
+        marginBottom: "20px",
+        color: "white"
       }}
     >
       <h2>{tournament.title}</h2>
 
       <p>Prize Pool: ₹{tournament.prizePool}</p>
 
-      <p>Status: {tournament.status}</p>
+      <p>Entry Fee: ₹{tournament.entryFee}</p>
 
       <p>Slots: {tournament.filledSlots}/{tournament.totalSlots}</p>
+
+      <p>📌 Status: {tournament.status}</p>
     </div>
   );
 }
