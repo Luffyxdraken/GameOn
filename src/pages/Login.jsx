4d068f409d1036ae14af4a3bb32a44a74c1fb299
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-22 19:31:39 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,57 @@
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#0f172a",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          background: "#1e293b",
+          padding: "30px",
+          borderRadius: "12px",
+          width: "300px",
+        }}
+      >
+        <h1>Login</h1>
 
-      <input
-        type="email"
-        placeholder="Email"
-      />
+        <input
+          type="email"
+          placeholder="Email"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "10px",
+          }}
+        />
 
-      <br />
-      <br />
+        <input
+          type="password"
+          placeholder="Password"
+          style={{
+            width: "100%",
+            padding: "10px",
+            marginBottom: "10px",
+          }}
+        />
 
-      <input
-        type="password"
-        placeholder="Password"
-      />
-
-      <br />
-      <br />
-
-      <button>
-        Login
-      </button>
+        <button
+          style={{
+            width: "100%",
+            padding: "10px",
+            background: "#f97316",
+            border: "none",
+            color: "white",
+          }}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
