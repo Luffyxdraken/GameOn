60260ad076075bfa1a767c32a5fed5a070989ce3
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-23 15:49:57 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,47 +3,53 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Tournament from "./pages/Tournament";
-import Admin from "./pages/Admin";
-
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
-import Messages from "./pages/Messages";
 import Members from "./pages/Members";
+import Messages from "./pages/Messages";
 import Guilds from "./pages/Guilds";
 import MyMatches from "./pages/MyMatches";
 import Notifications from "./pages/Notifications";
+import Admin from "./pages/Admin";
 import SuperAdmin from "./pages/SuperAdmin";
+import Tournament from "./pages/Tournament";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
+return (
+<BrowserRouter>
+<Routes>
 
-        {/* Public */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/tournament/:id" element={<Tournament />} />
+    {/* Public Pages */}
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
 
-        {/* User */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/members" element={<Members />} />
-        <Route path="/guilds" element={<Guilds />} />
-        <Route path="/mymatches" element={<MyMatches />} />
-        <Route path="/notifications" element={<Notifications />} />
+    {/* User Dashboard */}
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/members" element={<Members />} />
+    <Route path="/messages" element={<Messages />} />
+    <Route path="/guilds" element={<Guilds />} />
+    <Route path="/mymatches" element={<MyMatches />} />
+    <Route path="/notifications" element={<Notifications />} />
 
-        {/* Admin */}
-        <Route path="/admin" element={<Admin />} />
+    {/* Tournament */}
+    <Route
+      path="/tournament/:id"
+      element={<Tournament />}
+    />
 
-        {/* Super Admin */}
-        <Route path="/superadmin" element={<SuperAdmin />} />
+    {/* Admin */}
+    <Route path="/admin" element={<Admin />} />
+    <Route
+      path="/superadmin"
+      element={<SuperAdmin />}
+    />
 
-      </Routes>
-    </BrowserRouter>
-  );
+  </Routes>
+</BrowserRouter>
+
+);
 }
 
 export default App;
